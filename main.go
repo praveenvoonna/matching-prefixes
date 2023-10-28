5ee9d0b883ea1926d84d81168873d28fe3cf40e1
Praveen Voonna <prasant.voonna@dsm.com>
2023-10-28 23:50:58 +0530

    input from terminal

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"matching_prefixes/prefixes"
+	"os"
 )
 
 func main() {
-	prefixes.LoadPrefixes("prefixes.txt")
-	input := "mDHjp"
+	err := prefixes.LoadPrefixes("prefixes.txt")
+	if err != nil {
+		fmt.Println("Error loading prefixes from prefixes.txt file")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the input string: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reader read string from terminal")
+	}
+
+	input = input[:len(input)-1]
+
 	fmt.Println("Input String:", input)
 	longestPrefix := prefixes.FindLongestPrefix(input)
 	fmt.Println("Longest Matching Prefix:", longestPrefix)
--- tests/prefixes_test.go
+++ tests/prefixes_test.go
@@ -6,32 +6,34 @@ import (
 )
 
 func TestFindLongestPrefix(t *testing.T) {
-	// Test cases with actual matches
-	prefixes.LoadPrefixes("../prefixes.txt")
+	// Load prefixes for testing
+	err := prefixes.LoadPrefixes("../prefixes.txt")
+	if err != nil {
+		t.Errorf("Error loading prefixes: %s", err)
+	}
 
-	// Test case 1
+	// Test case 1: Test with a matching prefix
 	input := "mDHjpabcderhjkvv"
 	expected := "mDHjp"
 	result := prefixes.FindLongestPrefix(input)
 	if result != expected {
-		t.Errorf("Test failed, expected: '%s', got: '%s'", expected, result)
+		t.Errorf("Test 1 failed, expected: '%s', got: '%s'", expected, result)
 	}
 
-	// Test case 2
+	// Test case 2: Test with another matching prefix
 	input = "ePX0Gqwavvjhhdtjgj"
 	expected = "ePX0Gqwav"
 	result = prefixes.FindLongestPrefix(input)
 	if result != expected {
-		t.Errorf("Test failed, expected: '%s', got: '%s'", expected, result)
+		t.Errorf("Test 2 failed, expected: '%s', got: '%s'", expected, result)
 	}
 
-	// Test cases without matches
-	// Test case 3
+	// Test case 3: Test with no matching prefix
 	input = "apple"
 	expected = ""
 	result = prefixes.FindLongestPrefix(input)
 	if result != expected {
-		t.Errorf("Test failed, expected: '%s', got: '%s'", expected, result)
+		t.Errorf("Test 3 failed, expected: '%s', got: '%s'", expected, result)
 	}
 
 	// Test case 4 - Empty input
@@ -39,7 +41,7 @@ func TestFindLongestPrefix(t *testing.T) {
 	expected = ""
 	result = prefixes.FindLongestPrefix(input)
 	if result != expected {
-		t.Errorf("Test failed, expected: '%s', got: '%s'", expected, result)
+		t.Errorf("Test 4 failed, expected: '%s', got: '%s'", expected, result)
 	}
 
 	// Test case 5
@@ -47,7 +49,7 @@ func TestFindLongestPrefix(t *testing.T) {
 	expected = ""
 	result = prefixes.FindLongestPrefix(input)
 	if result != expected {
-		t.Errorf("Test failed, expected: '%s', got: '%s'", expected, result)
+		t.Errorf("Test 5 failed, expected: '%s', got: '%s'", expected, result)
 	}
 
 }
